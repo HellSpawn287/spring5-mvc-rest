5038ccc5331277c44c82ae3d552166433fd46dfd
Robert Prus <robert.prus87@gmail.com>
2019-11-03 22:26:42 +0100

    Externalize API url value

--- src/test/java/guru/springfamework/controllers/v1/CategoryControllerTest.java
+++ src/test/java/guru/springfamework/controllers/v1/CategoryControllerTest.java
@@ -56,7 +56,7 @@ public class CategoryControllerTest {
         when(categoryService.getAllCategories()).thenReturn(categories);
 
         mockMvc.perform(
-                get("/api/v1/categories/")
+                get(CategoryController.BASE_URL + "/")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.categories", hasSize(2)));
@@ -71,7 +71,7 @@ public class CategoryControllerTest {
         when(categoryService.getCategoryByName(anyString())).thenReturn(category1);
 
         mockMvc.perform(
-                get("/api/v1/categories/Jim")
+                get(CategoryController.BASE_URL + "/Jim")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", equalTo(NAME)));
